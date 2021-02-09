8338ff2a7fb43ef0d64a1f6dfdb639eba1a8f005
khalil-Elf441 <elfallahikhalil@gmail.com>
2021-02-09 17:12:47 +0100

    add transaction signature

--- wallet.py
+++ wallet.py
@@ -1,5 +1,6 @@
-from Crypto import PublicKey
 from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA512
 import Crypto.Random 
 import binascii
 
@@ -38,4 +39,19 @@ class Wallet:
     def generate_keys(self):
         private_key = RSA.generate(1024, Crypto.Random.new().read)
         public_key = private_key.publickey()
-        return (binascii.hexlify(private_key.export_key(format='DER')).decode('ascii'),binascii.hexlify(public_key.export_key(format='DER')).decode('ascii'))
+        return (binascii.hexlify(private_key.export_key(format='DER')).decode('ascii'),
+        binascii.hexlify(public_key.export_key(format='DER')).decode('ascii'))
+
+    def sign_transaction(self, t_from, t_to, amount):
+        signer = PKCS1_v1_5.new(RSA.importKey(binascii.unhexlify(self.private_key)))
+        tx_hash = SHA512.new((str(t_from)+str(t_to)+str(amount)).encode('utf8'))
+        signature = signer.sign(tx_hash)
+        return binascii.hexlify(signature).decode('ascii')
+
+    @staticmethod
+    def verify_transaction(transaction):
+        public_key = RSA.importKey(binascii.unhexlify(transaction.t_from))
+        verifier = PKCS1_v1_5.new(public_key)
+        h = SHA512.new((str(transaction.t_from) + str(transaction.t_to) + str(transaction.amount)).encode('utf8'))
+        return verifier.verify(h, binascii.unhexlify(transaction.signature))
+        
